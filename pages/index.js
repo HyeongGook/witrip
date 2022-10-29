5517670b7d5c7c9beb6edc4d2efdb7f980f44f73
Seochangh2 <nomm89@naver.com>
2022-10-29 14:12:37 +0900

    init

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Main from "./main";
+
+export default function Home() {
+  return <Main></Main>;
+}
---
+++ pages/main.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Main.module.css";
+const Main = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}></div>
+    </div>
+  );
+};
+
+export default Main;

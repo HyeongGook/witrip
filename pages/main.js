6a113548c30d2f288acfb5215aa91c9be4b67fd7
Seochangh2 <nomm89@naver.com>
2022-10-30 00:07:38 +0900

    기초 페이지 생성

--- pages/main.js
+++ pages/main.js
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import styles from "../styles/Main.module.css";
 const Main = () => {
+  const [copyCoupon, setCopy] = useState("EH2DSK8SJDKS7SJDLS4SJD3SDM9");
+  const [isCopy, setIsCopy] = useState(false);
+  const handleCopyClipBoard = async () => {
+    try {
+      await navigator.clipboard.writeText(copyCoupon);
+      setCopy("쿠폰번호 복사 성공!");
+      setIsCopy(true);
+    } catch (error) {}
+  };
   return (
     <div className={styles.container}>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <div className={styles.titleContainer}>
+          <p className={styles.title}>우리끼리, 위트 있는 여행{`\n`}witrip</p>
+        </div>
+        <div className={styles.explainContainer}>
+          <p className={styles.explain}>아직 출시 준비중인 서비스입니다.</p>
+          <p className={styles.explain}>
+            출시 후, 앱에 다음 쿠폰을 입력하시면 할인 쿠폰 등 사전 혜택을
+            드리겠습니다!
+          </p>
+          <p className={styles.explain}>관심가져주셔서 감사합니다!</p>
+        </div>
+        <div className={styles.couponContainer}>
+          <input
+            readOnly
+            value={copyCoupon}
+            onClick={() => {
+              if (!isCopy) handleCopyClipBoard();
+            }}
+            className={styles.coupon}
+          ></input>
+        </div>
+      </div>
     </div>
   );
 };
